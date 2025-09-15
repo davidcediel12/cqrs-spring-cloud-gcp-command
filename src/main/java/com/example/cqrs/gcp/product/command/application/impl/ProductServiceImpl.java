a61c632cf597379b6f667cd9ae5c2719643114b5
David Cediel <esdago18@gmail.com>
2025-09-15 15:13:11 -0500

    Add service implementation

--- src/main/java/com/example/cqrs/gcp/product/command/application/impl/ProductServiceImpl.java
+++ src/main/java/com/example/cqrs/gcp/product/command/application/impl/ProductServiceImpl.java
@@ -1,21 +1,53 @@
 package com.example.cqrs.gcp.product.command.application.impl;
 
 import com.example.cqrs.gcp.product.command.application.ProductService;
+import com.example.cqrs.gcp.product.command.domain.entity.Product;
+import com.example.cqrs.gcp.product.command.domain.exception.ProductNotFoundException;
+import com.example.cqrs.gcp.product.command.domain.repository.ProductImageRepository;
+import com.example.cqrs.gcp.product.command.domain.repository.ProductRepository;
 import com.example.cqrs.gcp.product.command.infrastructure.dto.ProductDto;
+import com.example.cqrs.gcp.product.command.mapper.ProductMapper;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.annotation.Validated;
 
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+@Slf4j
+@Validated
 public class ProductServiceImpl implements ProductService {
+
+
+    private final ProductRepository productRepository;
+    private final ProductImageRepository productImageRepository;
+    private final ProductMapper productMapper;
+
     @Override
-    public ProductDto create(ProductDto productDto) {
-        return null;
+    public ProductDto create(@NotNull @Valid ProductDto productDto) {
+
+        Product product = productMapper.toEntity(productDto);
+        product = productRepository.save(product);
+
+        return productMapper.toDto(product);
     }
 
     @Override
-    public void delete(Long productId) {
+    public void delete(@NotNull Long productId) {
 
+        productRepository.deleteById(productId);
     }
 
     @Override
-    public ProductDto get(Long productId) {
-        return null;
+    public ProductDto get(@NotNull Long productId) {
+
+        return productRepository.findById(productId)
+                .map(productMapper::toDto)
+                .orElseThrow(() -> new ProductNotFoundException(String.format("Product with id %s not found", productId)));
     }
 }
