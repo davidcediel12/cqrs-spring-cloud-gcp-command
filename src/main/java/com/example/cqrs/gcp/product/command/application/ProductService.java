6d23600c5276f3aa6fc033a988c2baa02a3bdaa1
David Cediel <esdago18@gmail.com>
2025-09-15 15:43:49 -0500

    Put service method's validations in the interface

--- src/main/java/com/example/cqrs/gcp/product/command/application/ProductService.java
+++ src/main/java/com/example/cqrs/gcp/product/command/application/ProductService.java
@@ -1,13 +1,15 @@
 package com.example.cqrs.gcp.product.command.application;
 
 import com.example.cqrs.gcp.product.command.infrastructure.dto.ProductDto;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 
 public interface ProductService {
 
 
-    ProductDto create(ProductDto productDto);
+    ProductDto create(@NotNull @Valid ProductDto productDto);
 
-    void delete(Long productId);
+    void delete(@NotNull Long productId);
 
-    ProductDto get(Long productId);
+    ProductDto get(@NotNull Long productId);
 }
